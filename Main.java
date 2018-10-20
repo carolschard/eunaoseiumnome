546bb65c831df016eead3ae736e3eecda379e8ca
Giovani Rubim Francisco <giovanirubim.f@gmail.com>
2018-10-20 03:29:01 -0300

    Com scanner

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		System.out.println("Insira a expressao");
+		Scanner scanner = new Scanner(System.in);
 		Parser parser = new Parser();
-		Arvore arvore = parser.parse(reader.readLine());
+		Arvore arvore = parser.parse(scanner.next());
 		arvore.imprimir();
+		System.out.println();
 	}
 }
